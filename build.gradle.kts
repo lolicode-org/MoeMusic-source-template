a38f439ce8783b0fc6a51f82237c67e62f437f97
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-08-17 09:17:24 +0000

    build: version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.3.21"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21"
-    id("com.gradleup.shadow") version "9.4.1"
+    id("org.jetbrains.kotlin.jvm") version "2.4.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10"
+    id("com.gradleup.shadow") version "9.6.1"
     id("idea")
 }
 
